1d8680911b7fbdbbc5151cf414681646a79690b9
Innakqa <innakokoreva@gmail.com>
2021-01-31 20:43:04 -0500

    more movie1 branch, updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
         System.out.println("Hello World, b21-new");
         System.out.println("B21 ... SDETs");
-
         //test under main
+        // watching more movie in bedroom
+        // watching more movie in bedroom
     }
 }
