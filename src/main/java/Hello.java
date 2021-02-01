1d2a15b32a5d858beb3069675014b490e6eb5d92
Innakqa <innakokoreva@gmail.com>
2021-01-31 20:34:22 -0500

    movie1 branch, updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World, b21-new");
         System.out.println("B21 ... SDETs");
 
         //test under main
