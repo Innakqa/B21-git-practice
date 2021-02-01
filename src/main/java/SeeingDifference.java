2e2f2df6937e98fc4359a0a602bf23c42fd828ca
Innakqa <innakokoreva@gmail.com>
2021-02-01 00:17:06 -0500

    master added note

--- src/main/java/SeeingDifference.java
+++ src/main/java/SeeingDifference.java
@@ -4,4 +4,6 @@ public class SeeingDifference {
     //    //This is the new line of work in master that movie5 doesnt know about
     //This is the new line of work in movie5
 
+    //have no idea what to do
+
 }
